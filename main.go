748fc585263bd323c2b7faa22ab7ee5071818c37
PBGlory <deadglory1@yandex.com>
2025-03-05 06:46:28 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -8,65 +8,47 @@ import (
 	"net/http"
 )
 
-var task string
-
 func GetHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Получен GET-запрос")
 	var tasks []Task
 
 	if err := DB.Find(&tasks).Error; err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		http.Error(w, "Ошибка получения данных", http.StatusInternalServerError)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 
 	if err := json.NewEncoder(w).Encode(tasks); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		http.Error(w, "Ошибка кодирования ответа", http.StatusInternalServerError)
 	}
-
 }
 
 func PostHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Получен POST-запрос")
 
-	var requestBody struct {
-		Task   string `json:"task"`
-		IsDone bool   `json:"is_Done"`
-	}
+	var newTask Task
 
-	err := json.NewDecoder(r.Body).Decode(&requestBody)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	if requestBody.Task == "" {
-		http.Error(w, "Поле task обязательно", http.StatusBadRequest)
+	if err := json.NewDecoder(r.Body).Decode(&newTask); err != nil {
+		http.Error(w, "Ошибка декодирования JSON", http.StatusBadRequest)
 		return
 	}
 
-	task := Task{
-		Task:   requestBody.Task,
-		IsDone: requestBody.IsDone,
+	if newTask.Task == "" {
+		http.Error(w, "Поле 'task' обязательно", http.StatusBadRequest)
+		return
 	}
 
-	if err := DB.Create(&task).Error; err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	if err := DB.Create(&newTask).Error; err != nil {
+		http.Error(w, "Ошибка сохранения в БД", http.StatusInternalServerError)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 
-	response := map[string]string{
-		"message": fmt.Sprintf("Задача успешно создана: %s", task.Task),
-	}
+	json.NewEncoder(w).Encode(newTask)
 
-	if err := json.NewEncoder(w).Encode(response); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
 }
 
 func main() {
